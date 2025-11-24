08040f074803dc420a7d34f99d4294220f850625
muzamilhussain33 <muzamilhussain369@gmail.com>
2025-11-24 20:09:22 +0500

    add GA4 measurement id

--- src/main.jsx
+++ src/main.jsx
@@ -4,6 +4,10 @@ import App from "./App.jsx";
 import './assets/css/bootstrap.min.css';
 import './assets/css/style.css';
 import './assets/css/responsive.css';
+import ReactGA from "react-ga4";
+
+// Initialize once when the app starts
+ReactGA.initialize("G-Q8DBG8JBYL");
 
 ReactDOM.createRoot(document.getElementById("root")).render(
     <React.StrictMode>
