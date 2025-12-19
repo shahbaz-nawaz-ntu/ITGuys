69b470f9dc09f1c82ab185afdaf9dac45dc09d2d
muzamilhussain33 <muzamilhussain369@gmail.com>
2025-12-19 17:13:19 +0500

    add call now button

--- src/components/BackToTop.jsx
+++ src/components/BackToTop.jsx
@@ -1,7 +1,8 @@
 import React, { useEffect } from 'react';
-import { Link  } from 'react-router-dom';
+import { Link } from 'react-router-dom';
 import TopToBottom from '../lib/TopToBottom.js';
 import whatsappicon from "../assets/whatsappicon.png";
+import { GrPhone } from "react-icons/gr";
 
 function BackToTop({ className }) {
     useEffect(() => {
@@ -9,21 +10,35 @@ function BackToTop({ className }) {
     }, []); // Added empty dependency array for proper effect cleanup\
 
     const handleQuoteClick = () => {
-  if (window.dataLayer) {
-    window.dataLayer.push({
-      event: "quote_button_click",
-    });
-  }
+        if (window.dataLayer) {
+            window.dataLayer.push({
+                event: "quote_button_click",
+            });
+        }
 
-  // existing functionality (WhatsApp / popup / scroll)
-};
+        // existing functionality (WhatsApp / popup / scroll)
+    };
 
     return (
         <>
-            
+
             <div className={`scroll-to-top d-flex flex-column align-items-center gap-2${className || ''}`}>
-                <Link to="https://wa.me/971588996975" onClick={handleQuoteClick}><img className="whatsapp" src={whatsappicon}/>
-                <div className='fs-9'>Chat Now</div>
+                <Link
+                    to="tel:+971588996975"
+                    onClick={handleQuoteClick}
+                    className='d-flex flex-column align-items-center text-decoration-none text-black transition-colors duration-300 hover:text-[var(--theme-color2)]'
+                >
+                    <GrPhone size={30} color='green' />
+                    <div className='fs-9'>Call Now</div>
+                </Link>
+
+                <Link
+                    to="https://wa.me/971588996975"
+                    onClick={handleQuoteClick}
+                    className='d-flex flex-column align-items-center text-decoration-none text-black transition-colors duration-300 hover:text-[var(--theme-color2)]'
+                >
+                    <img className="whatsapp" src={whatsappicon} alt="WhatsApp" />
+                    <div className='fs-9'>Chat Now</div>
                 </Link>
                 <Link className='scrol-top-btn' to="#">
                     <i className="fa fa-angle-up" style={{ color: '#fff' }} />
